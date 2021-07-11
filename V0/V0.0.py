53f74e213d238aa38c28431b9df0aeb460f82fc0
Diego Espindola <59180229+BrXiaoz@users.noreply.github.com>
2021-07-11 16:31:37 -0300

    Calculadora V0.0
    Próximo passo estou pensando em utilizar um for para não ter que reiniciar o programa quando digitar um número errado

--- V0/V0.0.py
+++ V0/V0.0.py
@@ -1,1 +1,34 @@
-d
+
+numero1 = int(input('Insira o primeiro número: '));
+numero2 = int(input('Insira o segundo número: '));
+
+operacao = int(input('Selecione a operação:\nDigite:\n 1-Soma\n 2-Subtração\n 3-Divisão\n 4-Multiplicação\n '));
+
+resultado = 0;
+
+
+if operacao==1:
+
+	resultado = numero1+numero2;
+
+	print('O resultado é: ' resultado);
+
+elif operacao==2:
+	
+	resultado = numero1-numero2;
+
+	print('O resultado é: ' resultado);
+
+elif operacao==3:
+
+	resultado = numero1/numero2;
+
+	print('O resultado é: ' resultado);
+
+elif operacao==4:
+
+	resultado = numero1*numero2;
+
+	print('O resultado é: ' resultado);
+
+else: print('Reinicie e digite um número válido');
