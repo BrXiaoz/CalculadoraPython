487e90cf8fc5f9b3b091307a5f0870fc447a5577
Diego Espindola <diegoespindola9404@gmail.com>
2021-07-13 12:47:14 -0300

    Create V0.1.py
    Caso erre a operação irá aparecer novamente o pedido até a pessoa digitar o número correto

---
+++ V0/V0.1.py
@@ -0,0 +1,37 @@
+
+numero1 = int(input('Insira o primeiro número: '));
+numero2 = int(input('Insira o segundo número: '));
+
+operacao = int(input('Selecione a operação:\nDigite:\n 1-Soma\n 2-Subtração\n 3-Divisão\n 4-Multiplicação\n '));
+
+while (operacao>4) or (operacao<1):
+	
+	operacao = int(input('Selecione uma operação válida:\nDigite:\n 1-Soma\n 2-Subtração\n 3-Divisão\n 4-Multiplicação\n '));
+
+
+resultado = 0;
+
+
+if operacao==1:
+
+	resultado = numero1+numero2;
+
+	print('O resultado é: ' + str(resultado));
+
+elif operacao==2:
+	
+	resultado = numero1-numero2;
+
+	print('O resultado é: ' + str(resultado));
+
+elif operacao==3:
+
+	resultado = numero1/numero2;
+
+	print('O resultado é: ' + str(resultado));
+
+elif operacao==4:
+
+	resultado = numero1*numero2;
+
+	print('O resultado é: ' + str(resultado));
